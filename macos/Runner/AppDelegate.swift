fd8177bd1edac9a3256c70dd23bc6e45259f1be4
fufesou <linlong1266@gmail.com>
2025-04-16 22:53:08 +0800

    test
    Signed-off-by: fufesou <linlong1266@gmail.com>

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -1,9 +1,35 @@
 import Cocoa
 import FlutterMacOS
+import Sparkle
 
 @main
 class AppDelegate: FlutterAppDelegate {
+  private let updaterController = SPUStandardUpdaterController(startingUpdater: true, updaterDelegate: nil, userDriverDelegate: nil)
+
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
     return true
+  }
+
+  func getUpdaterController() -> SPUStandardUpdaterController {
+      return updaterController
+  }
+  
+  override func applicationDidFinishLaunching(_ notification: Notification) {
+    print("======================== applicationDidFinishLaunching start updater ")
+    
+    // let controller = self.getUpdaterController()
+    // controller.startUpdater()
+    
+    // let flutterViewController = mainFlutterWindow.contentViewController as! FlutterViewController
+    // GeneratedPluginRegistrant.register(with: self)
+
+    print("======================== applicationDidFinishLaunching set up the updater controller done")
+    
+    // Set up the updater
+    // controller.checkForUpdates(nil)
+
+    // controller.checkForUpdatesInBackground()
+
+    print("======================== applicationDidFinishLaunching end updater ")
   }
 }
